89162c99d41e1fcc1bf3a132ad78c25d584c716d
art1975 <46888552+art1975@users.noreply.github.com>
2019-01-23 13:03:41 +0100

    Create parking.php

---
+++ parking.php
@@ -0,0 +1,22 @@
+<?php
+ 
+require 'connection.php';
+$conn   = Connect();
+$light  = $conn->real_escape_string($_POST['light']);
+$temp   = $conn->real_escape_string($_POST['temp']);
+$bat    = $conn->real_escape_string($_POST['bat']);
+$btn    = $conn->real_escape_string($_POST['btn']);
+$query  = "INSERT into parking (light,temp,bat,btn) VALUES('" . $light . "','" . $temp . "','" . $bat . "','" . $btn . "')";
+$success = $conn->query($query);
+ 
+if (!$success) {
+    die("Couldn't enter data: ".$conn->error);
+ 
+}
+ 
+echo "Thanks for data";
+  
+ 
+$conn->close();
+ 
+?>
